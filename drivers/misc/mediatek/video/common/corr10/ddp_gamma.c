928013741b64afcb4418565c643bbcb48fc28644
flar2 <asegaert@gmail.com>
2017-03-10 00:10:55 -0500

    RGB color control cherry-picked from MT6797
    https://github.com/flar2/Elephone-S7
    
    Adapted from work of @defer
    https://github.com/CyanogenMod/android_kernel_cyanogen_mt6735/commit/c0cfb516de9a114601a4269c136e599ee368fe47
    
    For compatibility with various kernel apps, changed to widely used kcal sysfs path and rescaled to 256

--- drivers/misc/mediatek/video/common/corr10/ddp_gamma.c
+++ drivers/misc/mediatek/video/common/corr10/ddp_gamma.c
@@ -228,10 +228,18 @@ static int disp_gamma_set_lut(const struct DISP_GAMMA_LUT_T __user *user_gamma_l
 		return -EFAULT;
 	}
 
+#ifdef CONFIG_MTK_KCAL_CTRL
+	if (virt_addr_valid(user_gamma_lut)) {
+		memcpy(gamma_lut, user_gamma_lut, sizeof(struct DISP_GAMMA_LUT_T));
+	} else
+#endif
 	if (copy_from_user(gamma_lut, user_gamma_lut, sizeof(struct DISP_GAMMA_LUT_T)) != 0) {
+		GAMMA_ERR("disp_gamma_set_lut: cannot copy from user mem");
 		ret = -EFAULT;
 		kfree(gamma_lut);
-	} else {
+	}
+
+	if (!ret) {
 		id = gamma_lut->hw_id;
 		if (id >= 0 && id < DISP_GAMMA_TOTAL) {
 			mutex_lock(&g_gamma_global_lock);
